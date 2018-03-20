c8b9dcb945c443db747002eea02efaa0e8f3dbd4
Jared Palmer <jared@palmer.net>
2018-03-20 14:14:56 -0400

    Update for mwa ;)

--- index.js
+++ index.js
@@ -1,78 +1,88 @@
-const fetch = require('node-fetch')
-const ms = require('ms')
+const fetch = require('node-fetch');
+const ms = require('ms');
 
-let data = []
+let data = [];
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET')
-  return data
-}
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  return data;
+};
 
 // Cache data now and every X ms
-cacheData()
-setInterval(cacheData, ms('15m'))
+cacheData();
+setInterval(cacheData, ms('15m'));
 
 function log(text) {
-  return slack(text, process.env.TOKEN_EVENTS)
+  return slack(text, process.env.TOKEN_EVENTS);
 }
 
 function logError(text) {
-  return slack(text, process.env.TOKEN_ALERTS)
+  return slack(text, process.env.TOKEN_ALERTS);
 }
 
 function slack(text, id) {
   fetch(`https://hooks.slack.com/services/${id}`, {
     method: 'POST',
-    body: JSON.stringify({text})
-  })
+    body: JSON.stringify({ text }),
+  });
 }
 
 function cacheData() {
-  const start = Date.now()
-  fetch('https://api.github.com/orgs/zeit/repos?per_page=100', {
+  const start = Date.now();
+  fetch('https://api.github.com/orgs/jaredpalmer/repos?per_page=100', {
     headers: {
-      Accept: 'application/vnd.github.preview'
-    }
+      Accept: 'application/vnd.github.preview',
+    },
   })
-  .then(res => {
-    if (res.status !== 200) {
-      return logError('Non-200 response code from GitHub: ' + res.status)
-    }
-    return res.json()
-  })
-  .then(data_ => {
-    if (!data_) {
-      return
-    }
-
-    // Ugly hack because github sometimes doesn't return
-    // all the right search results :|
-    let featured = 0
-    data_.forEach(({name}) => {
-      if (name === 'hyper' || name === 'next.js' || name === 'micro') {
-        featured++
+    .then(res => {
+      if (res.status !== 200) {
+        return logError('Non-200 response code from GitHub: ' + res.status);
       }
+      return res.json();
     })
+    .then(data_ => {
+      if (!data_) {
+        return;
+      }
 
-    if (featured !== 3) {
-      return logError(`Error: GitHub did not include all projects (${featured})`)
-    }
+      // Ugly hack because github sometimes doesn't return
+      // all the right search results :|
+      let featured = 0;
+      data_.forEach(({ name }) => {
+        if (
+          name === 'razzle' ||
+          name === 'backpack' ||
+          name === 'react-fns' ||
+          name === 'after.js' ||
+          name === 'formik'
+        ) {
+          featured++;
+        }
+      });
 
-    data = data_.map(({name, description, stargazers_count, html_url}) => ({
-      name,
-      description,
-      url: html_url,
-      stars: stargazers_count
-    })).sort((p1, p2) =>
-      p2.stars - p1.stars
-    )
+      if (featured !== 3) {
+        return logError(
+          `Error: GitHub did not include all projects (${featured})`
+        );
+      }
 
-    log(`Re-built projects cache. ` +
-        `Total: ${data.length} public ▲ZEIT projects. ` +
-        `Elapsed: ${(new Date() - start)}ms`)
-  })
-  .catch(err => {
-    logError('Error parsing response from GitHub: ' + err.stack)
-  })
+      data = data_
+        .map(({ name, description, stargazers_count, html_url }) => ({
+          name,
+          description,
+          url: html_url,
+          stars: stargazers_count,
+        }))
+        .sort((p1, p2) => p2.stars - p1.stars);
+
+      log(
+        `Re-built projects cache. ` +
+          `Total: ${data.length} public projects. ` +
+          `Elapsed: ${new Date() - start}ms`
+      );
+    })
+    .catch(err => {
+      logError('Error parsing response from GitHub: ' + err.stack);
+    });
 }
--- readme.md
+++ readme.md
@@ -1,10 +1,6 @@
 # github-repos
 
-[![Build Status](https://travis-ci.org/zeit/github-repos.svg?branch=master)](https://travis-ci.org/zeit/github-repos)
-[![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/sindresorhus/xo)
-[![Slack Channel](http://zeit-slackin.now.sh/badge.svg)](https://zeit.chat/)
-
-Microservice to cache and expose GitHub projects for [this page](https://zeit.co/oss).
+Microservice to cache and expose GitHub projects for Jared Palmer
 
 ## Usage
 
@@ -32,20 +28,20 @@ npm run dev
     "name": "project-name",
     "description": "The description woot",
     "stars": 3040,
-    "url": "https://github.com/zeit/test"
+    "url": "https://github.com/jaredpalmer/test"
   }
 ]
 ```
 
 ## Contributing
 
-1. [Fork](https://help.github.com/articles/fork-a-repo/) this repository to your own GitHub account and then [clone](https://help.github.com/articles/cloning-a-repository/) it to your local device
-2. Follow the [usage section](#usage)
-3. Start making changes and open a pull request once they're ready!
+1.  [Fork](https://help.github.com/articles/fork-a-repo/) this repository to your own GitHub account and then [clone](https://help.github.com/articles/cloning-a-repository/) it to your local device
+2.  Follow the [usage section](#usage)
+3.  Start making changes and open a pull request once they're ready!
 
 You can use `npm test` to run the tests and see if your changes have broken anything.
 
 ## Authors
 
-- Guillermo Rauch ([@rauchg](https://twitter.com/rauchg)) - [▲ZEIT](https://zeit.co)
-- Leo Lamprecht ([@notquiteleo](https://twitter.com/notquiteleo)) - [▲ZEIT](https://zeit.co)
+* Guillermo Rauch ([@rauchg](https://twitter.com/rauchg)) - [▲ZEIT](https://zeit.co)
+* Leo Lamprecht ([@notquiteleo](https://twitter.com/notquiteleo)) - [▲ZEIT](https://zeit.co)
